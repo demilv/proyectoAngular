5de024af049c5b286c1444e9b5e949eaec9f6785
demilv <gonzalo.cano@bootcamp-upgrade.com>
2023-07-02 14:05:54 +0200

    Empezando un poco con la parte frontal

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: `home`, loadChildren: () =>
+      import('./components/home/home.module').then(m => m.HomeModule)
+  },
+  {
+    path: `form`, loadChildren: () =>
+      import('./components/form/form.module').then(m => m.FormModule)
+  },
+  {
+    path: `list`, loadChildren: () =>
+      import('./components/list/list.module').then(m => m.ListModule)
+  },
+  { path: ``, redirectTo: `home`, pathMatch: `full` }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
